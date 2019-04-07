4b4d6c64091f572dfd3a6c2fe40301248851521d
Caster22 <kopaczmichal093@gmail.com>
2019-04-07 22:13:52 +0200

    dodano zjd

--- CarSale/models.py
+++ CarSale/models.py
@@ -4,9 +4,11 @@ from django.utils import timezone
 class Post(models.Model):
 	author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 	title = models.CharField(max_length=400)
+	text_opis = models.TextField()
 	text = models.TextField()
+	image = models.ImageField(upload_to = 'image/')
 	published_date = models.DateTimeField(auto_now_add=True)
-
+	
 
 	def publish_car(self):
 		self.published_date = timezone.now()
